721fbecb3b15cae900243ebe35fc1b3b215bcc84
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2023-06-19 11:51:25 +0700

    feat(parameter): add migration of paramter tables

---
+++ database/migrations/2023_06_18_084931_create_parameters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParametersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parameters', function (Blueprint $table) {
+            $table->id();
+            $table->string('position', 10)->unique();
+            $table->tinyInteger('bonus_percentage');
+            $table->tinyInteger('pph_percentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parameters');
+    }
+}
